04fe4aa0cd3d25ef7447ff859fcfb824a66aac11
MelaniaPahovska <melania.pashovska@students.finki.ukim.mk>
2024-01-19 19:50:57 +0100

    test

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 
-const mobileBackgroundColor = Color.fromRGBO(0, 0, 0, 1);
+const mobileBackgroundColor = Color.fromRGBO(240, 237, 237, 1);
 const webBackgroundColor = Color.fromRGBO(18, 18, 18, 1);
 const mobileSearchColor = Color.fromRGBO(38, 38, 38, 1);
 const blueColor = Color.fromRGBO(54, 2, 165, 1);
-const greenColor = Color.fromRGBO(1, 129, 48, 1);
-const primaryColor = Colors.grey;
+const greenColor = Color.fromRGBO(103, 137, 131, 1);
+const primaryColor = Color.fromRGBO(30, 30, 29, 1);
 const secondaryColor = Color.fromARGB(255, 244, 237, 237);
+const mediumGrey = Color.fromRGBO(134, 138, 133, 1);
+const warmGrey = Color.fromRGBO(103, 89, 75, 1);
+const coolGrey = Color.fromRGBO(203, 199, 197, 1);
+const blueGrey = Color.fromRGBO(25, 30, 50, 1);
